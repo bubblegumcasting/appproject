b4f1cb359ff073bdfdf03fc24d95afd620fd80c1
bubblegumcasting <casting@bubblegumcasting.com>
2014-11-25 09:13:31 -0800

    changed main page

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-class HomeController extends BaseController {
+class IndexController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
@@ -15,9 +15,9 @@ class HomeController extends BaseController {
 	|
 	*/
 
-	public function showWelcome()
+	public function showIndex()
 	{
-		return View::make('hello');
+		return View::make('index');
 	}
 
 }
---
+++ app/views/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Base for App Project</title>
+</head>
+<body>
+	
+</body>
+</html>
