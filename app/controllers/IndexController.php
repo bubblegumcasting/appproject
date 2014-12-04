da4b08c2ea43a9fed0ddb429006035c944b419f1
bubblegumcasting <info@bubblegumcasting.com>
2014-12-04 12:54:28 -0800

    Revert "minor tweak"
    This reverts commit f2d2c6301bb6d1744ace0d217d26bfc50d30cb86.

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,8 +4,15 @@ class IndexController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
-	| Default Index Controller
+	| Default Home Controller
 	|--------------------------------------------------------------------------
+	|
+	| You may wish to use controllers instead of, or in addition to, Closure
+	| based routes. That's great! Here is an example controller method to
+	| get you started. To route to this controller, just add the route:
+	|
+	|	Route::get('/', 'HomeController@showWelcome');
+	|
 	*/
 
 	public function showIndex()
