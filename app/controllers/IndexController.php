1bcf49cd96ef4b308fe830c4baae1f31536bfa6f
bubblegumcasting <info@bubblegumcasting.com>
2014-12-04 12:56:07 -0800

    minor tweak
    a minor change

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -4,15 +4,8 @@ class IndexController extends BaseController {
 
 	/*
 	|--------------------------------------------------------------------------
-	| Default Home Controller
+	| Default Index Controller
 	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
 	*/
 
 	public function showIndex()
